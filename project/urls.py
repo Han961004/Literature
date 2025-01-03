ec4236ac2ba5551bd186a6774eb49990d6609a41
Dong-Han <donghk77@naver.com>
2025-01-03 16:34:23 +0900

    feat : update urls

--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from ..accounts.views.login import LoginView
+from ..accounts.views.logout import LogoutView
+from ..accounts.views.user import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # accounts
+    path('v1/accounts/create/', UserView.as_view()),
+    path('v1/accounts/login/', LoginView.as_view()),
+    path('v1/accounts/logout/', LogoutView.as_view()),
 ]
