cffcc5b65f8c50f39e0e3bb5d3ae6855d76d58a6
Dong-Han <donghk77@naver.com>
2025-01-03 17:17:18 +0900

    chore : corsheaders

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
