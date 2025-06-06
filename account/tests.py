36fe83c91ff0c338da05e5a1941b0ffe74cbbc41
Dong-Han <donghk77@naver.com>
2025-06-06 21:39:38 +0900

    init commit

--- account/tests.py
+++ account/tests.py
@@ -23,7 +23,7 @@ class Tests(APITestCase):
         self.user2.save()
         Profile.objects.create(user=self.user2, nickname='user2')
     
-    # 권한
+    # 권한 Helper 함수
     def authenticate(self):
         res = self.client.post(self.login_url, {"email": "user1@example.com", "password": "password123"})
         self.assertEqual(res.status_code, status.HTTP_200_OK)
@@ -31,14 +31,14 @@ class Tests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
 
     # 회원 가입 및 프로필 생성 성공
-    def user_join_success(self):
+    def test_user_join_success(self):
         data = {"email": "new@example.com", "password": "newpassword123"}
         res = self.client.post(self.join_url, data)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertTrue(User.objects.filter(email="new@example.com").exists())
     
     # 회원 가입 실패
-    def user_join_fail(self):
+    def test_user_join_fail(self):
         data = {"email": "user1@example.com", "password": "short"}
         res = self.client.post(self.join_url, data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -46,7 +46,7 @@ class Tests(APITestCase):
     # 회원 정보 수정 성공(패스워드 변경)
     
     # 회원 탈퇴 성공
-    def user_delete_success(self):
+    def test_user_delete_success(self):
         self.authenticate()
         url = reverse('user_delete')
         res = self.client.delete(url)
@@ -57,14 +57,14 @@ class Tests(APITestCase):
     # 프로필 정보 수정 실패
     
     # 로그인 성공
-    def login_success(self):
+    def test_login_success(self):
         data = {"email": "user1@example.com", "password": "password123"}
         res = self.client.post(self.login_url, data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn("token", res.data['response'])
 
     # 로그인 실패
-    def login_fail(self):
+    def test_login_fail(self):
         data = {"email": "user1@example.com", "password": "wrongpass"}
         res = self.client.post(self.login_url, data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
