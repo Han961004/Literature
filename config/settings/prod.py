206082191741cda5bf791798af3565f0522b6d84
Dong-Han <donghk77@naver.com>
2025-06-07 20:15:35 +0900

    init commit

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = False
 
@@ -21,9 +22,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
-DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # 운영 환경 보안 강화
 SESSION_COOKIE_SECURE = True
